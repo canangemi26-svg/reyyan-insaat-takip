11cedd16319a57d0cf0d640a79fd8ea3db9c9898
canangemi26-svg <canangemi26@icloud.com>
2026-09-14 13:00:44 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase bağlantı bilgilerini buraya yazın.
+// service_role anahtarını ASLA buraya koymayın; yalnızca publishable/anon key kullanın.
+window.REYYAN_CONFIG = {
+  supabaseUrl: "https://woqxbdixzpwuyvqbsmlo.supabase.co/rest/v1/",
+  supabaseKey: "sb_publishable_vW7gVS0eS8nPOWDpOklYfQ_7THKWmcY"
+};
